ba8197e1c40d182fa9c1e9a1804b466eee477872
Raphael Saldanha <rfsaldanha@gmail.com>
2022-02-25 17:43:11 -0300

    Population UF comment

--- data-raw/population_uf.R
+++ data-raw/population_uf.R
@@ -1,5 +1,7 @@
 ## code to prepare `population_uf` dataset goes here
 
+# CSV created observing https://www.gov.br/pt-br/noticias/financas-impostos-e-gestao-publica/2021/08/populacao-brasileira-chega-a-213-3-milhoes-de-habitantes-estima-ibge
+
 population_uf <- readr::read_csv2(file = "data-raw/pop_uf_2021.csv")
 
 usethis::use_data(population_uf, overwrite = TRUE)
