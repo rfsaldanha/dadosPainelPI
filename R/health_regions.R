84c62455ff3c5ad85f672a614a454cfc96e90ae4
Raphael Saldanha <rfsaldanha@gmail.com>
2022-03-03 08:35:43 -0300

    Document health regions table.

---
+++ R/health_regions.R
@@ -0,0 +1,10 @@
+#' Health regions table
+#'
+#' Table of Brazilian municipalities and health regions codes, considering the 450 health regions version.
+#'
+#' @format A data frame with 5,570 rows and 2 variables:
+#' \describe{
+#'   \item{cod_mun}{Municipality code from IBGE, with 6 digits.}
+#'   \item{cod_rs_450}{Health region code.}
+#' }
+"health_regions"
