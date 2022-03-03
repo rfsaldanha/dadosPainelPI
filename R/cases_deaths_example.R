1bea3ca106e18ead7ec68628e769bb6cd3eea647
Raphael Saldanha <rfsaldanha@gmail.com>
2022-03-03 08:35:33 -0300

    Document cases and deaths example table.

---
+++ R/cases_deaths_example.R
@@ -0,0 +1,6 @@
+#' Covid-19 cases and deaths example table
+#'
+#' Example table with Covid-19 cases and deaths, considering data from the Brazilian Health Ministry from February 23, 2022.
+#'
+#' @format A data frame with 100,000 rows and 17 columns.
+"cases_deaths_example"
