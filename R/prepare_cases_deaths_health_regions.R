451cde97edd5a3fac095c93fa91025e440dc3aff
Raphael Saldanha <rfsaldanha@gmail.com>
2022-03-03 09:17:41 -0300

    Health regions cases and deaths table with rate option.

--- R/prepare_cases_deaths_health_regions.R
+++ R/prepare_cases_deaths_health_regions.R
@@ -2,15 +2,18 @@
 #'
 #' @param cases_deaths A tibble from read_cases_deaths
 #' @param uf_code UF code. Defaults to 22.
+#' @param with_rate Logical. Calculate per capita rate.
+#' @param constant Numerical. Constant for rate.
+#' @param digits Numerical. Digits for rates.
 #' @return A tibble.
 #' @importFrom rlang .data
-prepare_cases_deaths_health_regions <- function(cases_deaths, uf_code = 22){
+prepare_cases_deaths_health_regions <- function(cases_deaths, uf_code = 22, with_rate = TRUE, constant = 100000, digits = 2){
 
   # Health regions from a UF
   subset_health_regions <- dadosPainelPI::health_regions %>%
     dplyr::filter(substr(.data$cod_mun, start = 0, stop = 2) == uf_code)
 
-  cases_deaths %>%
+  res <- cases_deaths %>%
     # Inner join from health regions and municipalities
     dplyr::inner_join(y = subset_health_regions, by = c("codmun" = "cod_mun")) %>%
     # Select and rename variables
@@ -19,5 +22,17 @@ prepare_cases_deaths_health_regions <- function(cases_deaths, uf_code = 22){
       new_cases = .data$casosNovos, acum_cases = .data$casosAcumulado,
       new_deaths = .data$obitosNovos, acum_deaths = .data$obitosAcumulado
     )
+
+  if(with_rate == TRUE){
+    res <- res %>%
+      dplyr::left_join(y = dadosPainelPI::population_health_region, by = c("cod_rs" = "cod_rs_450")) %>%
+      dplyr::mutate(
+        acum_cases_rate = round((.data$acum_cases / .data$pop2021)*constant, digits),
+        acum_deaths_rate = round((.data$acum_deaths / .data$pop2021)*constant, digits)
+      ) %>%
+      dplyr::select(-.data$pop2021)
+  }
+
+  return(res)
 
 }
