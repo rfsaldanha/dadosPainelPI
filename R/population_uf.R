3d686cd1d3deb2b0311bea790b51175b3623105a
Raphael Saldanha <rfsaldanha@gmail.com>
2022-03-03 08:35:57 -0300

    Document UF population table.

---
+++ R/population_uf.R
@@ -0,0 +1,11 @@
+#' Population UF table
+#'
+#' Table of UF population estimatives with data from IBGE for 2021.
+#'
+#' @format A data frame with 27 rows and 3 variables:
+#' \describe{
+#'   \item{uf_code}{UF code from IBGE.}
+#'   \item{uf}{UF acronym.}
+#'   \item{pop2021}{Population estimative for 2021.}
+#' }
+"population_uf"
