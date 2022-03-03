dfe4be44f056e7427cf7f067a68db5869645b24a
Raphael Saldanha <rfsaldanha@gmail.com>
2022-03-03 11:18:32 -0300

    Change example data localization.

--- data-raw/cases_deaths_example.R
+++ data-raw/cases_deaths_example.R
@@ -1,6 +1,6 @@
 ## code to prepare `cases_deaths_example` dataset goes here
 
-cases_deaths_example <- read_cases_deaths(files_folder = "/Users/raphaelsaldanha/Downloads/HIST_PAINEL_COVIDBR_23fev2022/") %>%
+cases_deaths_example <- read_cases_deaths(files_folder = "data-raw/cases_deaths_example/") %>%
   dplyr::filter(regiao != "Brasil") %>%
   head(100000)
 
