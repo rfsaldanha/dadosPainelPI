eabca575081c9e4a7619749fb6ea77fd48616ce6
Raphael Saldanha <rfsaldanha@gmail.com>
2022-03-08 11:04:38 -0300

    Consistency check for health regions indicator.

--- tests/testthat/test-prepare_cases_deaths_health_regions.R
+++ tests/testthat/test-prepare_cases_deaths_health_regions.R
@@ -4,3 +4,13 @@ test_that("prepare cases and deaths file for health regions", {
 
   expect_true(nrow(res) > 0)
 })
+
+test_that("check consistency", {
+  res <- cases_deaths_example %>%
+    prepare_cases_deaths_health_regions()
+
+  res_dis <- res %>%
+    dplyr::distinct(date, cod_rs)
+
+  expect_true(nrow(res) == nrow(res_dis))
+})
