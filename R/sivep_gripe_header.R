be2a0b81d882df4328c7d63f310b7216c623352d
Raphael Saldanha <rfsaldanha@gmail.com>
2022-03-10 15:05:38 -0300

    Function to read Sivep Gripe files to a database.

---
+++ R/sivep_gripe_header.R
@@ -0,0 +1,5 @@
+#' Sivep Gripe header
+#'
+#' Tibble with header, empty lines. Internal use.
+#'
+"sivep_gripe_header"
---
+++ data-raw/sivep_gripe_example.R
@@ -0,0 +1,3 @@
+## code to prepare `sivep_gripe_example` dataset goes here
+
+usethis::use_data(sivep_gripe_example, overwrite = TRUE)
