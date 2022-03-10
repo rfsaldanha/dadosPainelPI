24f7d4cc6bc7950caaca0cdc1d313ecc39d3e3da
Raphael Saldanha <rfsaldanha@gmail.com>
2022-03-10 10:43:01 -0300

    Documentation.

--- R/prepare_and_export_data_csv.R
+++ R/prepare_and_export_data_csv.R
@@ -1,20 +1,20 @@
 #' Prepare and export data to CSV files
 #
-#' @param files_folder Folder with cases and deaths files.
+#' @param cases_deaths_folder Folder with cases and deaths files.
 #' @param dest_folder Destination folder for CSV files.
-#' @param year_start Start year. Numeric.
-#' @param year_end End year. Numeric.
-#' @param month_start Start month. Numeric.
-#' @param month_end End month. Numeric.
+#' @param year_start Start year. Numeric. For SIH data.
+#' @param year_end End year. Numeric. For SIH data.
+#' @param month_start Start month. Numeric. For SIH data.
+#' @param month_end End month. Numeric. For SIH data.
 #' @return A tibble.
 #' @importFrom rlang .data
-prepare_and_export_data_csv <- function(files_folder = "data-raw/cases_deaths_example/", dest_folder, year_start = 2021, year_end = 2021, month_start = 1, month_end = 1){
+prepare_and_export_data_csv <- function(cases_deaths_folder = "data-raw/cases_deaths_example/", dest_folder, year_start = 2021, year_end = 2021, month_start = 1, month_end = 1){
   # Cases and deaths
   message("Cases and deaths...")
 
   time_date_stamp <- format(Sys.time(), "%Y%m%d_%H%M%S")
 
-  db_cases_deaths <- read_cases_deaths(files_folder)
+  db_cases_deaths <- read_cases_deaths(cases_deaths_folder)
 
   db_cases_deaths %>%
     prepare_cases_deaths_uf() %>%
