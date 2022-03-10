d372b183fa5b6556c755856903c4a0471089e6af
Vanderlei Matos <vanderlei.matos@gmail.com>
2022-03-10 17:03:32 -0300

    teste R GIT

--- R/population_uf.R
+++ R/population_uf.R
@@ -2,7 +2,7 @@
 #'
 #' Table of UF population estimatives with data from IBGE for 2021.
 #'
-#' @format A data frame with 27 rows and 3 variables:
+#' @format A data frame with 27 rows and 3 variables.:
 #' \describe{
 #'   \item{uf_code}{UF code from IBGE.}
 #'   \item{uf}{UF acronym.}
