bdda51195443acb768a504fd3411fe2ad2852d53
Raphael Saldanha <rfsaldanha@gmail.com>
2022-03-17 13:53:34 -0300

    Prepare vaccination data for municipalities.

---
+++ R/prepare_vaccination_municipalities.R
@@ -0,0 +1,27 @@
+#' Prepare a tibble with vaccination counts for municipalities within a UF.
+#
+#' @param lazy_sipni Lazi table from `read_sipni` function.
+#' @param uf_acronym UF acronym. Defaults to PI
+#' @return A tibble.
+#' @importFrom rlang .data
+
+prepare_vaccination_municipalities <- function(lazy_sipni, uf_acronym = "PI"){
+  lazy_sipni %>%
+    dplyr::filter(.data$paciente_endereco_uf == uf_acronym) %>%
+    dplyr::mutate(vacina_descricao_dose = dplyr::case_when(
+      .data$vacina_descricao_dose == "1\u00aa Dose" ~ "dose_1",
+      .data$vacina_descricao_dose == "1\u00aa Dose Revacina\u00e7\u00e3o" ~ "dose_1",
+      .data$vacina_descricao_dose == "2\u00aa Dose" ~ "dose_2",
+      .data$vacina_descricao_dose == "2\u00aa Dose Revacina\u00e7\u00e3o" ~ "dose_2",
+      .data$vacina_descricao_dose == "3\u00aa Dose" ~ "dose_3",
+      .data$vacina_descricao_dose == "Dose Adicional" ~ "dose_3",
+      .data$vacina_descricao_dose == "Refor\u00e7o" ~ "dose_3"
+    )) %>%
+    dplyr::group_by(date = .data$vacina_dataAplicacao, cod_mun = .data$paciente_endereco_coIbgeMunicipio, .data$vacina_descricao_dose) %>%
+    dplyr::summarise(freq = dplyr::n()) %>%
+    dplyr::ungroup() %>%
+    dplyr::collect() %>%
+    dplyr::mutate(freq = as.numeric(.data$freq)) %>%
+    tidyr::pivot_wider(id_cols = c(.data$date, .data$cod_mun), names_from = .data$vacina_descricao_dose, values_from = .data$freq, values_fill = 0) %>%
+    dplyr::select(.data$date, .data$cod_mun, .data$dose_1, .data$dose_2, .data$dose_3, -.data$`NA`)
+}
